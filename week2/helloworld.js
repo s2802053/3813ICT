38166492267007a276243f5936bfaf9bfcf34a4b
Joe <j.burton93@outlook.com>
2019-08-07 23:12:46 +1000

    Test

--- week2/helloworld.js
+++ week2/helloworld.js
@@ -1,11 +1,8 @@
-adding_function
-console.log("I fixed the merge conflict!");
+console.log("123");
 
 function addTwo(x, y){
     return x + y;
 }
 
 console.log(addTwo(5, 10));
-=======
-console.log("I fixed the merge conflict!");
-master
+
